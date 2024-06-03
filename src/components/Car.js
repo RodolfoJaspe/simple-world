c66d618ce5eb2d1dc2c33d87776a7d90b271fce3
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2024-06-02 22:38:25 -0400

    wheels rotating, front wheels wrong rotation

--- src/components/Car.js
+++ src/components/Car.js
@@ -3,9 +3,9 @@ import { RigidBody } from '@react-three/rapier';
 import React, { forwardRef } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
+
 const Car = forwardRef(({ camera }, ref) => {
     const gltf = useLoader(GLTFLoader, '/Assets/car/scene.gltf');
-
     // Enable shadows on the car mesh
     gltf.scene.traverse((child) => {
         if (child.isMesh) {
@@ -14,40 +14,33 @@ const Car = forwardRef(({ camera }, ref) => {
         }
     });
 
-    const rightFrontRim = gltf.scene.getObjectByName('right_front_rim');
-    const leftFrontRim = gltf.scene.getObjectByName('left_front_rim');
-    const rearRims = gltf.scene.getObjectByName('rear_rims');
-    const rightFrontTire = gltf.scene.getObjectByName('right_front_tire');
-    const leftFrontTire = gltf.scene.getObjectByName('left_front_tire');
-    const rearTires = gltf.scene.getObjectByName('rear_tires');
 
+    // Extract wheels for rotation
     const wheels = {
-        rightFrontRim:rightFrontRim,
-        leftFrontRim:leftFrontRim,
-        rearRims:rearRims,
-        rightFrontTire:rightFrontTire,
-        leftFrontTire:leftFrontTire,
-        rearTires:rearTires
-    }
-
-    // console.log(wheels)
+        leftFrontRim: gltf.scene.getObjectByName('left_front_rim'),
+        leftFrontTire: gltf.scene.getObjectByName('left_front_tire'),
+        rightFrontRim: gltf.scene.getObjectByName('right_front_rim'),
+        rightFrontTire: gltf.scene.getObjectByName('right_front_tire'),
+        rearRims: gltf.scene.getObjectByName('rear_rims'),
+        rearTires: gltf.scene.getObjectByName('rear_tires'),
+    };
 
     return (
         <RigidBody 
             type="dynamic" 
-            position={[0, 5, 0]} 
+            position={[0, 1, -1]} 
             rotation={[0, Math.PI / 2, 0]} 
             colliders="hull" 
             mass={10000}
             linearDamping={1} 
             angularDamping={5} 
             gravityScale={2}
-            ref={ref}
             userData={wheels}
+            ref={ref}
         >
             <primitive object={gltf.scene} scale={1} castShadow receiveShadow />
         </RigidBody>
     );
 });
 
-export default Car;
+export default Car
